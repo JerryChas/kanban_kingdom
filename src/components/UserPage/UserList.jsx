815ff33e6cfe80d4141520b13f5732a6aa12ec91
Moa Sandberg <moisan1999@gmail.com>
2024-05-10 11:04:54 +0200

    fixed bugg

--- src/components/UserPage/UserList.jsx
+++ src/components/UserPage/UserList.jsx
@@ -1,45 +1,48 @@
-import { useDispatch } from "react-redux";
-import { removeUser } from "../../slices/UserSlice";
-import css from "./UserList.module.css";
+import { useDispatch } from 'react-redux';
+import { removeUser } from '../../slices/UserSlice';
+import css from './UserList.module.css';
 
-const UserList = ({ users }) => {
-  const dispatch = useDispatch();
+const UserList = () => {
+	const dispatch = useDispatch();
 
-  // Dispatch the removeUser action with the userId
-  const handleRemoveUser = (userId) => {
-    dispatch(removeUser(userId));
-  };
+	// Dispatch the removeUser action with the userId
+	const handleRemoveUser = (userId) => {
+		dispatch(removeUser(userId));
+	};
 
-  return (
-    <>
-      <div className={css.user_list_container}>
-        <p>Users</p>
-        <ul className={css.ul}>
-          {users.map((user) => (
-            <li className={css.li} key={user.id}>
-              <div className={css.left}>
-                <img
-                  className={css.img}
-                  src={user.profilePhoto}
-                  alt={user.name}
-                />{" "}
-                <p>{user.name}</p>
-              </div>
-              <div className={css.right}>
-                <p>Added {user.dateJoined}</p>
-                <button
-                  className="user_page_button"
-                  onClick={() => handleRemoveUser(user.id)} // Call handleRemoveUser with userId
-                >
-                  Remove
-                </button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
-  );
+	// Hämta användarna från LocalStorage
+	const users = JSON.parse(localStorage.getItem('users')) || [];
+
+	return (
+		<>
+			<div className={css.user_list_container}>
+				<p>Users</p>
+				<ul className={css.ul}>
+					{users.map((user) => (
+						<li className={css.li} key={user.id}>
+							<div className={css.left}>
+								<img
+									className={css.img}
+									src={user.profilePhoto}
+									alt={user.name}
+								/>{' '}
+								<p>{user.name}</p>
+							</div>
+							<div className={css.right}>
+								<p>Added {user.dateJoined}</p>
+								<button
+									className="user_page_button"
+									onClick={() => handleRemoveUser(user.id)} // Call handleRemoveUser with userId
+								>
+									Remove
+								</button>
+							</div>
+						</li>
+					))}
+				</ul>
+			</div>
+		</>
+	);
 };
 
 export default UserList;
